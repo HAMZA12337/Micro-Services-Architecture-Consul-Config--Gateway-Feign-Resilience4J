21cfba0ff5a51a2d4fdcdb1976f41fabfecf9546
Your Name <you@example.com>
2023-10-27 09:51:24 +0100

    first One

---
+++ customer-servuce/src/main/java/org/sid/customerservuce/CustomerServuceApplication.java
@@ -0,0 +1,13 @@
+package org.sid.customerservuce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServuceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServuceApplication.class, args);
+	}
+
+}
---
+++ customer-servuce/src/test/java/org/sid/customerservuce/CustomerServuceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.customerservuce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServuceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
